73444f546937c691e57517f7b30bd726fb1128d7
Helen Hosse <helenhosse@gmail.com>
2023-01-03 22:33:50 -0700

    fixed top of page and saved answers

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// adding the time at the top of the page
-
 $(document).ready(function () {
     
     var displayTime = $("#currentTime");
@@ -8,49 +6,55 @@ $(document).ready(function () {
     var clearBtn = $(".clearBtn");
     var saveAllBtn = $("#saveAll");
     var clearAllBtn = $("#clearAll")
-});
+
+    function timeBlock() {
+        var hour = moment().hours();
+        console.log (timeBlock);
+    
+        $(".time-block").each(function() {
+            var currentTime = parseInt($(this).attr("id"));
+            if (currentTime > hour) {
+                $(this).addClass("future");
+            } else if (currentTime === hour) {
+                $(this).addClass("present");
+            } else {
+                $(this).addClass("past");
+            }
+            timeBlock();
+        })
+    };
+// this will show the time at the top of the page
+    displayTime.html(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+    setInterval(()=> {
+        displayTime.html(moment().format('MMMM Do YYYY, h:mm:ss a'));
+        timeBlock();
+    }, 1000);
 
 
-("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
-
-function timeBlock() {
-    var hour = moment().hours();
-
-    console.log (timeBlock);
-
-    $(".time-block").each(function() {
-        var currentTime = parseInt($(this).attr("id"));
-        if (currrentTime > hour) {
-            $(this).addClass("future");
-        } else if (currrentTime === hour) {
-            $(this).addClass("present");
-        } else {
-            $(this).addClass("past");
-        }
-    })
-};
-
-var saveBtn = $(".saveBtn");
-
-saveBtn.on ("click", function() {
-    var time = $(this).siblings(".hour").text();
-    var data = $(this).siblings(".data").val();
-
-    localStorage.setItem(time, data);
-});
-
-function saveInput() {
-    console.log(saveInput);
-
-    $(".hour").each(function() {
-        var currentHour = $(this).text();
-        var currData = localStorage.getItem(currentHour);
-
-        if(currData !== null) {
-            $(this).siblings(".data").val(currData);
-        }
+    saveBtn.on ("click", function() {
+        var time = $(this).siblings(".hour").text();
+        var data = $(this).siblings(".data").val();
+    
+        localStorage.setItem(time, data);
     });
-}
+    
+    function saveInput() {
+        console.log(saveInput);
+    
+        $(".hour").each(function() {
+            var currentTime = $(this).text();
+            var currData = localStorage.getItem(currentTime);
+    
+            if(currData !== null) {
+                $(this).siblings(".data").val(currData);
+            }
+        });
+        saveInput();
+    }
 
-timeBlock();
-saveInput();
+});
+
+
+
+
